abb80d0aac38d7ab5e892a55d52c633fc1dc57d5
miyamoto <ryohei1000000000@gmail.com>
2021-11-16 22:37:58 +0900

    add db

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.4
+	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.10.4
 	golang.org/x/crypto v0.0.0-20211115234514-b4de73f9ece8 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	gorm.io/gorm v1.22.3
 )
--- src/main.go
+++ src/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+
+    db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+    if err != nil {
+        log.Fatalf("Error opening database: %q", err)
+    }
+    fmt.Println(db)
+
+
+
     engine:= gin.Default()
     engine.GET("/", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
